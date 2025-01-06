1aa29c29b2e566c3b9d67dd8c4b1f67fa8736562
Jevon Putra <jevonop88@gmail.com>
2025-01-06 19:33:14 +0700

    Add new screen and view model for detail coin

---
+++ app/src/main/java/com/jop/cointracker/view/detailCoin/screen/DetailCoinScreen.kt
@@ -0,0 +1,11 @@
+package com.jop.cointracker.view.detailCoin.screen
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import com.jop.cointracker.view.detailCoin.viewModel.DetailCoinViewModel
+
+@Composable
+fun DetailCoinScreen(navHostController: NavHostController, detailVM: DetailCoinViewModel = hiltViewModel()){
+
+}
