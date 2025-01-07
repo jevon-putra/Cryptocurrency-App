4d16ec78911e89e44e54bc72cfb7241aab6f150f
Jevon Putra <jevonop88@gmail.com>
2025-01-07 17:40:40 +0700

    Add new endpoint for get detail coin and get price history

--- app/src/main/java/com/jop/cointracker/network/api/CoinAPI.kt
+++ app/src/main/java/com/jop/cointracker/network/api/CoinAPI.kt
@@ -3,7 +3,10 @@ package com.jop.cointracker.network.api
 import com.jop.cointracker.data.model.BaseResponse
 import com.jop.cointracker.data.model.Coin
 import com.jop.cointracker.data.model.CoinData
+import com.jop.cointracker.data.model.CoinDetailData
+import com.jop.cointracker.data.model.CoinHistoryPriceData
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CoinAPI {
@@ -12,4 +15,11 @@ interface CoinAPI {
                          @Query("offset") offset: Int,
                          @Query("symbols[]") symbols: List<String>,
                          @Query("tags[]") tags: List<String>) : BaseResponse<CoinData>
+
+    @GET("v2/coin/{uuid}")
+    suspend fun getCoinDetail(@Path("uuid") uuid: String) : BaseResponse<CoinDetailData>
+
+    @GET("v2/coin/{uuid}/history")
+    suspend fun getCoinHistoryPrice(@Path("uuid") uuid: String,
+                                    @Query("timePeriod") timePeriod: String) : BaseResponse<CoinHistoryPriceData>
 }
