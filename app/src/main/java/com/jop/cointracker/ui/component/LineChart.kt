b413bd4a01f0e0d75f04c8224bf9e50ee40ffef9
Jevon Putra <jevonop88@gmail.com>
2025-01-07 17:42:14 +0700

    Update logic color uptrend or downtren in line chart

--- app/src/main/java/com/jop/cointracker/ui/component/LineChart.kt
+++ app/src/main/java/com/jop/cointracker/ui/component/LineChart.kt
@@ -4,14 +4,10 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
@@ -21,27 +17,29 @@ import androidx.compose.ui.graphics.asComposePath
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.jop.cointracker.ui.theme.errorLight
-import com.jop.cointracker.ui.theme.primaryDark
 
 @Composable
-fun LineChart(modifier: Modifier = Modifier, list: List<Double> = listOf()){
+fun LineChart(modifier: Modifier = Modifier, list: List<Double> = listOf(), isShowDifference: Boolean = false){
     val zipList: List<Pair<Double, Double>> = list.zipWithNext()
-    val avg = list.average()
+    val first = list.first()
+    val last = list.last()
     val max = list.max()
     val min = list.min()
-    val spacing = 5f
-    val upTrend = list.last() >= avg
 
     Row(
         modifier = modifier
     ) {
         for (pair in zipList) {
+            val color = if((!isShowDifference && last > first) || (isShowDifference && pair.second > first)) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.error
             val fromValuePercentage = getValuePercentageForRange(pair.first, max, min)
             val toValuePercentage = getValuePercentageForRange(pair.second, max, min)
+
             Canvas(
-                modifier = Modifier.fillMaxHeight().weight(1f),
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f),
                 onDraw = {
+                    val spacing = size.width / list.size
                     val fromPoint = Offset(x = 0f, y = size.height.times(1 - fromValuePercentage).toFloat())
                     val toPoint = Offset(x = size.width, y = size.height.times(1 - toValuePercentage).toFloat())
 
@@ -52,7 +50,7 @@ fun LineChart(modifier: Modifier = Modifier, list: List<Double> = listOf()){
 
                     drawPath(
                         path = strokePath,
-                        color = if(upTrend) primaryDark else errorLight,
+                        color = color,
                         style = Stroke(
                             width = 1.dp.toPx(),
                             cap = StrokeCap.Round
@@ -70,10 +68,9 @@ fun LineChart(modifier: Modifier = Modifier, list: List<Double> = listOf()){
                         fillPath,
                         brush = Brush.verticalGradient(
                             listOf(
-                                if(upTrend) primaryDark else errorLight,
+                                color,
                                 Color.Transparent,
                             ),
-                            endY = size.height
                         ),
                     )
                 }
@@ -82,8 +79,7 @@ fun LineChart(modifier: Modifier = Modifier, list: List<Double> = listOf()){
     }
 }
 
-private fun getValuePercentageForRange(value: Double, max: Double, min: Double) =
-    (value - min) / (max - min)
+private fun getValuePercentageForRange(value: Double, max: Double, min: Double) = (value - min) / (max - min)
 
 @Preview
 @Composable
